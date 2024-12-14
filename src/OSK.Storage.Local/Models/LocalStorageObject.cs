4fb738dd749db4e07794cc7c24016f49ada0771e
BlankDev117 <mmmasent@gmail.com>
2024-12-13 19:56:44 -0500

    Pass cancellation token to ToArrayAsync

--- src/OSK.Storage.Local/Models/LocalStorageObject.cs
+++ src/OSK.Storage.Local/Models/LocalStorageObject.cs
@@ -77,7 +77,7 @@ namespace OSK.Storage.Local.Models
         private async ValueTask<byte[]> ReadStreamBytesAsync(CancellationToken cancellationToken)
         {
             using var contentStream = await GetContentAsMemoryStreamAsync(cancellationToken);
-            return await contentStream.ToArrayAsync();
+            return await contentStream.ToArrayAsync(cancellationToken: cancellationToken);
         }
 
         private async ValueTask<MemoryStream> GetContentAsMemoryStreamAsync(CancellationToken cancellationToken)
