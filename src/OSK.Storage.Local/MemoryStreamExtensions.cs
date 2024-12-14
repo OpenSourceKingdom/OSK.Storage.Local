a34624ef967f9f6e92e716345cb309c9a4366910
BlankDev117 <mmmasent@gmail.com>
2024-12-13 19:54:58 -0500

    Update for cancellation token

--- src/OSK.Storage.Local/MemoryStreamExtensions.cs
+++ src/OSK.Storage.Local/MemoryStreamExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OSK.Storage.Local
@@ -7,7 +8,7 @@ namespace OSK.Storage.Local
     public static class MemoryStreamExtensions
     {
         public static async Task<byte[]> ToArrayAsync(this MemoryStream memoryStream, int startIndex = 0, 
-            int bytesReadPerIteration = 1024)
+            int bytesReadPerIteration = 1024, CancellationToken cancellationToken = default)
         {
             memoryStream.Position = startIndex;
 
@@ -16,7 +17,7 @@ namespace OSK.Storage.Local
             do
             {
                 var byteReadCount = (int)Math.Min(bytesReadPerIteration, memoryStream.Length - startIndex - currentIndex);
-                await memoryStream.ReadAsync(byteArray, currentIndex, byteReadCount);
+                await memoryStream.ReadAsync(byteArray, currentIndex, byteReadCount, cancellationToken);
                 currentIndex += byteReadCount;
             } while (currentIndex + startIndex < memoryStream.Length);
 
